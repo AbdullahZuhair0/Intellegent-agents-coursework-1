438b91cd720d4f7fcfeb7bbd01b580daca5aa562
AbdullahZuhair0 <abdullahzuhairmc@gmail.com>
2025-10-06 21:08:10 +0400

    updated many things the game runs now but is not complete

--- sudoku.py
+++ sudoku.py
@@ -30,7 +30,7 @@ class cell:
         else:
             y += 1
         if x > 8:
-            return
+            return 
         return [x,y]
     def prev(self):
         x,y = self.x,self.y
@@ -40,7 +40,7 @@ class cell:
         else:
             y -= 1
         if x < 0:
-            return
+            return 
         return [x,y]
 
 # 2d array of rows and columns.
@@ -77,13 +77,13 @@ def findBlock(cel):
 cells = []
 row = []
 domain = ["1","2","3","4","5","6","7","8","9"]
-# adds cells (is wrong rn cuz duh)
+
 f = open("map.txt")
 for i in range(0,9):
     # adds 9 cells per row
     for j in range(0,9):
         char = f.read(1)
-        cel = cell(i, j, True if char != "." else False, char)
+        cel = cell(i, j,char != ".", char)
         row.append(cel)
     cells.append(row)
     row = []
@@ -97,48 +97,63 @@ we have added this to prevent cells from infinity trying the same guess.
 """
 while i < 9 and j < 9:
     currCell = cells[i][j]
-    currCell.addGuesses(domain,checkRCB(cells, currCell))
-    currCell.setValue(currCell.removeGuess())
+    if currCell.isClue == False:
+        currCell.addGuesses(domain,checkRCB(cells, currCell))
+        currCell.setValue(currCell.removeGuess())
     
     # if the original cell that didn't have a guess still doesn't have a value
     # could definitly be improved.
     while cells[i][j].value == ".":
         # if it's the first iteration
         if x == 0 and y == 0:
-            x,y = i-1, j-1
+            vals = cells[x][y].prev()
+            if vals is None:
+                break
+            x,y = vals[0], vals[1]
             currCell = cells[x][y]
-            GuessesNotEmpty.append(currCell)
-        if currCell.isClue == False:
-            currCell.setValue(currCell.removeGuess())
-        elif currCell.isClue == True and z == "next":
-            vals = currCell.prev()
+            GuessesNotEmpty.append(cells[x][y])
+        if cells[x][y].isClue == False:
+            cells[x][y].setValue(cells[x][y].removeGuess())
+        # if isClue
+        elif cells[x][y].isClue == True and z == "next":
+            vals = cells[x][y].prev()
+            if vals is None:
+                break
             x,y = vals[0], vals[1]
         else:
-            vals = currCell.next()
+            vals = cells[x][y].next()
+            if vals is None:
+                break
             x,y = vals[0], vals[1]
         # if even the previous cell is empty
-        if currCell.value == ".":
-            GuessesNotEmpty.pop(GuessesNotEmpty.index(currCell))
-            vals = currCell.prev()
-            x,y,z = vals[0], vals[1], "next"
+        if cells[x][y].value == ".":
+            GuessesNotEmpty.pop(GuessesNotEmpty.index(cells[x][y]))
+            if x == 0 and y == 0:
+                cells[x][y].addGuesses()
+                cells[x][y].setValue(cells[x][y].removeGuess())
+                y = 1
+            else:
+                vals = cells[x][y].prev()
+                if vals is None:
+                    break
+                x,y,z = vals[0], vals[1], "next"
         # go to the next cell
-        elif currCell.isClue == False:
-            vals = currCell.next()
+        elif cells[x][y].isClue == False:
+            vals = cells[x][y].next()
+            if vals is None:
+                break
             x,y,z = vals[0], vals[1], "prev"
         # whether we go back by 1 cell or we go to the next cell.
-        currCell = cells[x][y]
-        if currCell not in GuessesNotEmpty and currCell.isClue == False:
-            GuessesNotEmpty.append(currCell)
-            currCell.addGuesses(domain, checkRCB(cells, currCell))
+        if cells[x][y] not in GuessesNotEmpty and cells[x][y].isClue == False:
+            GuessesNotEmpty.append(cells[x][y])
+            cells[x][y].addGuesses(domain, checkRCB(cells, cells[x][y]))
     # end of nested while loop    
     GuessesNotEmpty,x,y = [], 0,0
     
         
-    if j == 8:
-        # if final row then exit
-        if i != 8:
-           i += 1
-           j = 0
+    if j == 8:    
+        i += 1
+        j = 0
     else:
         j += 1
 for row in cells:
